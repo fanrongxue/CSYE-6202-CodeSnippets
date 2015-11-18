4c6afe897ca63190bb45bf75d81fcff084136c6c
barisyanmaz <byanmaz@gmail.com>
2015-11-18 11:48:16 -0500

    WPF code snippet

---
+++ LogonScreenSln/LogonScreenApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿#region Using Directives
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+#endregion
+
+namespace LogonScreenApp
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void HandleSupermanMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{			
+			var uriSource = new Uri(@"/LogonScreenApp;component/images/SupermanPowerStats.png", UriKind.Relative);
+			SuperStatsImage.Source = new BitmapImage(uriSource);
+    }
+
+		private void HandleInvisibleWomanMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			var uriSource = new Uri(@"/LogonScreenApp;component/images/InvisibleWomanPowerStats.png", UriKind.Relative);
+			SuperStatsImage.Source = new BitmapImage(uriSource);
+		}
+
+		private void HandleHulkMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			var uriSource = new Uri(@"/LogonScreenApp;component/images/HulkPowerStats.png", UriKind.Relative);
+			SuperStatsImage.Source = new BitmapImage(uriSource);
+		}
+
+		private void HandleIronmanMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			var uriSource = new Uri(@"/LogonScreenApp;component/images/IronmanPowerStats.png", UriKind.Relative);
+			SuperStatsImage.Source = new BitmapImage(uriSource);
+		}
+	}
+}
