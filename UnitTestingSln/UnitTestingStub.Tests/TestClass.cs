a3443f094cc2e4d41ee3272a99e546521dc15f8d
barisyanmaz <byanmaz@gmail.com>
2015-11-18 11:45:04 -0500

    Checking in unit testing related code snippets

---
+++ UnitTestingSln/UnitTestingStub.Tests/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestingStub.Tests
+{
+	[TestFixture]
+	public class TestClass
+	{
+		Student student;
+		[TestFixtureSetUp]
+		public void Setup()
+		{
+			// create the student here..
+			student = new Student();
+		}
+
+		[Test]
+		public void When_StudentAgeAssigned_ResultShouldBeGreaterThanZero()
+		{
+			// prepare
+			var expected = 0;
+
+			// action	- this should be replaced with the actual method call
+			var actual = student.Age;
+
+			// assert
+			Assert.That(actual, Is.GreaterThanOrEqualTo(expected));
+
+
+		}
+	}
+}
---
+++ UnitTestingSln/UnitTestingStub/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingStub
+{
+	public class Student
+	{
+		public string StudentID { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Department { get; set; }
+		public string EnrollmentType { get; set; }
+
+		private int age;
+		public int Age { get { return -1;/*-1;*/ } set { age = value; } }
+
+		public Student() { }
+
+		public Student(string studentID, string firstName, string lastName, string department, string enrollmentType)
+		{
+			StudentID = studentID;
+			FirstName = firstName;
+			LastName = lastName;
+			Department = department;
+			EnrollmentType = enrollmentType;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("StudentID: {0}, FirstName: {1}, LastName: {2}, Department: {3}, EnrollmentType: {4}", StudentID, FirstName, LastName, Department, EnrollmentType);
+		}
+	}
+
+}
